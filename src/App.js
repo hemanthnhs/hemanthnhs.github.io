90686c65a04458f1d7b72c4e0b7cd7288026617f
Hemanth Sai Nimmala <hemanthsai10@gmail.com>
2021-01-17 01:08:08 -0500

    Left section layout including bio and links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Header from "./Header";
+import LeftContainer from "./LeftContainer";
+import RightContainer from "./RightContainer";
+import { Row, Col } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Row>
+          <Col md={4}>
+            <LeftContainer />
+          </Col>
+          <Col md={8}>
+            <RightContainer />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/RightContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class RightContainer extends React.Component {
+  render() {
+    return <div>Todo</div>;
+  }
+}
+
+export default RightContainer;
