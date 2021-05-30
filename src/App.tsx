f418a2454b2f3be50267868af57bea1055ae6e8a
Hemanth Sai <hemanthsai10@gmail.com>
2021-05-30 19:44:04 -0400

    Cleaning the old layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
 import "./App.css"
-import Header from "./components/Header"
-import ContentContainer from "./components/ContentContainer"
-import { Row, Col } from "antd"
-import { useState } from "react"
 
 const App = () => {
-    const [currentMenuInView, setMenuInView] = useState("about")
-    const isMobile = window.innerWidth < 480
     return (
         <div>
-            <Header isMobile={isMobile} currentMenuInView={currentMenuInView} />
-            <Row>
-                <Col offset={1} span={22}>
-                    <ContentContainer
-                        isMobile={isMobile}
-                        setMenuInView={setMenuInView}
-                    />
-                </Col>
-            </Row>
+            <>Todo</>
         </div>
     )
 }
